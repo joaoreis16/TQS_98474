5feb72a17f8ef27fbfeeff50c343e134aac02d1f
joaoreis16 <joaoreis16@ua.pt>
2022-03-22 16:48:13 +0000

    some updates

--- lab2/lab2_2/gs-mockForHttpClient/src/test/java/integration/AddressResolverIT.java
+++ lab2/lab2_2/gs-mockForHttpClient/src/test/java/integration/AddressResolverIT.java
@@ -18,6 +18,11 @@ public class AddressResolverIT {
 
     @BeforeEach
     public void init(){
+
+        private AddressResolver resolver;
+        private ISimpleHttpClient httpClient;
+        private String api_response = "{\"info\":{\"statuscode\":0,\"copyright\":{\"text\":\"\u00A9 2022 MapQuest, Inc.\",\"imageUrl\":\"http://api.mqcdn.com/res/mqlogo.gif\",\"imageAltText\":\"\u00A9 2022 MapQuest, Inc.\"},\"messages\":[]},\"options\":{\"maxResults\":1,\"thumbMaps\":true,\"ignoreLatLngInputv\":false},\"results\":[{\"providedLocation\":{\"latLng\":{\"lat\":40.6318,\"lng\":-8.658}},\"locations\":[{\"street\":\"Parque Estacionamento da Reitoria - Univerisdade de Aveiro\",\"adminArea6\":\"\",\"adminArea6Type\":\"Neighborhood\",\"adminArea5\":\"Gl\u00F3ria e Vera Cruz\",\"adminArea5Type\":\"City\",\"adminArea4\":\"\",\"adminArea4Type\":\"County\",\"adminArea3\":\"Centro\",\"adminArea3Type\":\"State\",\"adminArea1\":\"PT\",\"adminArea1Type\":\"Country\",\"postalCode\":\"3810-193\",\"geocodeQualityCode\":\"P1AAA\",\"geocodeQuality\":\"POINT\",\"dragPoint\":false,\"sideOfStreetv\":\"N\",\"linkId\":\"0\",\"unknownInput\":\"\",\"type\":\"s\",\"latLng\":{\"lat\":40.631803,\"lng\":-8.657881},\"displayLatLng\":{\"lat\":40.631803,\"lng\":-8.657881},\"mapUrl\":\"http://open.mapquestapi.com/staticmap/v5/map?key=uXSAVwYWbf9tJmsjEGHKKAo0gOjZfBLQ&type=map&size=225,160&locations=40.6318025,-8.657881|marker-sm-50318A-1&scalebar=true&zoom=15&rand=-472034917\",\"roadMetadata\":null}]}]}";
+    
     }
 
     @Test
@@ -27,6 +32,14 @@ public class AddressResolverIT {
 
         // repeat the same tests conditions from AddressResolverTest, without mocks
 
+        // Mockito.when( httpClient.doHttpGet( anyString() ) ).thenReturn( this.api_response );
+
+        //e.g.
+        Optional<Address> result = resolver.findAddressForLocation(40.640661, -8.656688);
+
+        //return
+        assertEquals( result, Optional.of( new Address( "Cais do Alboi", "Glória e Vera Cruz", "Centro", "3800-246", null) ) );
+
     }
 
     @Test
@@ -34,6 +47,12 @@ public class AddressResolverIT {
 
         //todo
         // repeat the same tests conditions from AddressResolverTest, without mocks
+
+        //todo
+        // Mockito.when( httpClient.doHttpGet( anyString() ) ).thenReturn( this.api_response );
+
+        //e.g.
+        assertThrows( IllegalArgumentException.class, () -> { Optional<Address> result = resolver.findAddressForLocation(-300, -810); } );
         
     }
 
