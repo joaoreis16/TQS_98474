18c1507fc32194883584e98e12fd80c9822989bc
joaoreis16 <joaoreis16@ua.pt>
2022-03-23 01:23:28 +0000

    ex3 - done

--- lab2/lab2_2/gs-mockForHttpClient/src/test/java/geocoding/AddressResolverTest.java
+++ lab2/lab2_2/gs-mockForHttpClient/src/test/java/geocoding/AddressResolverTest.java
@@ -2,7 +2,6 @@ package geocoding;
 
 import connection.ISimpleHttpClient;
 import org.json.simple.parser.ParseException;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -27,7 +26,7 @@ class AddressResolverTest {
     @Mock
     private ISimpleHttpClient httpClient;
 
-    private String api_response = "{\"info\":{\"statuscode\":0,\"copyright\":{\"text\":\"\\u00A9 2020 MapQuest, Inc.\",\"imageUrl\":\"http://api.mqcdn.com/res/mqlogo.gif\",\"imageAltText\":\"\\u00A9 2020 MapQuest, Inc.\"},\"messages\":[]},\"options\":{\"maxResults\":1,\"thumbMaps\":true,\"ignoreLatLngInput\":false},\"results\":[{\"providedLocation\":{\"latLng\":{\"lat\":40.6318,\"lng\":-8.658}},\"locations\":[{\"street\":\"Cais do Alboi\",\"adminArea6\":\"\",\"adminArea6Type\":\"Neighborhood\",\"adminArea5\":\"Gl\\u00F3ria e Vera Cruz\",\"adminArea5Type\":\"City\",\"adminArea4\":\"\",\"adminArea4Type\":\"County\",\"adminArea3\":\"Centro\",\"adminArea3Type\":\"State\",\"adminArea1\":\"PT\",\"adminArea1Type\":\"Country\",\"postalCode\":\"3800-246\",\"geocodeQualityCode\":\"P1AAA\",\"geocodeQuality\":\"POINT\",\"dragPoint\":false,\"sideOfStreet\":\"N\",\"linkId\":\"0\",\"unknownInput\":\"\",\"type\":\"s\",\"latLng\":{\"lat\":40.631803,\"lng\":-8.657881},\"displayLatLng\":{\"lat\":40.631803,\"lng\":-8.657881},\"mapUrl\":\"http://open.mapquestapi.com/staticmap/v5/map?key=uXSAVwYWbf9tJmsjEGHKKAo0gOjZfBLQ&type=map&size=225,160&locations=40.6318025,-8.657881|marker-sm-50318A-1&scalebar=true&zoom=15&rand=-563893378\",\"roadMetadata\":null}]}]}";
+    private String api_response = "{\"info\":{\"statuscode\":0,\"copyright\":{\"text\":\"\u00A9 2022 MapQuest, Inc.\",\"imageUrl\":\"http://api.mqcdn.com/res/mqlogo.gif\",\"imageAltText\":\"\u00A9 2022 MapQuest, Inc.\"},\"messages\":[]},\"options\":{\"maxResults\":1,\"thumbMaps\":true,\"ignoreLatLngInputv\":false},\"results\":[{\"providedLocation\":{\"latLng\":{\"lat\":40.6318,\"lng\":-8.658}},\"locations\":[{\"street\":\"Parque Estacionamento da Reitoria - Univerisdade de Aveiro\",\"adminArea6\":\"\",\"adminArea6Type\":\"Neighborhood\",\"adminArea5\":\"Gl\u00F3ria e Vera Cruz\",\"adminArea5Type\":\"City\",\"adminArea4\":\"\",\"adminArea4Type\":\"County\",\"adminArea3\":\"Centro\",\"adminArea3Type\":\"State\",\"adminArea1\":\"PT\",\"adminArea1Type\":\"Country\",\"postalCode\":\"3810-193\",\"geocodeQualityCode\":\"P1AAA\",\"geocodeQuality\":\"POINT\",\"dragPoint\":false,\"sideOfStreetv\":\"N\",\"linkId\":\"0\",\"unknownInput\":\"\",\"type\":\"s\",\"latLng\":{\"lat\":40.631803,\"lng\":-8.657881},\"displayLatLng\":{\"lat\":40.631803,\"lng\":-8.657881},\"mapUrl\":\"http://open.mapquestapi.com/staticmap/v5/map?key=uXSAVwYWbf9tJmsjEGHKKAo0gOjZfBLQ&type=map&size=225,160&locations=40.6318025,-8.657881|marker-sm-50318A-1&scalebar=true&zoom=15&rand=-472034917\",\"roadMetadata\":null}]}]}";
 
 
     @Test
@@ -45,7 +44,7 @@ class AddressResolverTest {
         Optional<Address> result = resolver.findAddressForLocation(40.640661, -8.656688);
 
         //return
-        assertEquals( result, Optional.of( new Address( "Cais do Alboi", "Glória e Vera Cruz", "Centro", "3800-246", null) ) );
+        assertEquals( result, Optional.of( new Address( "Parque Estacionamento da Reitoria - Univerisdade de Aveiro", "Glória e Vera Cruz", "Centro", "3810-193", null) ) );
 
     }
 
@@ -57,7 +56,7 @@ class AddressResolverTest {
         Mockito.when( httpClient.doHttpGet( anyString() ) ).thenReturn( this.api_response );
 
         //e.g.
-        assertThrows( IllegalArgumentException.class, () -> { Optional<Address> result = resolver.findAddressForLocation(-300, -810); } );
+        assertThrows( IllegalArgumentException.class, () -> { resolver.findAddressForLocation(-300, -810); } );
 
     }
 }
--- lab2/lab2_2/gs-mockForHttpClient/src/test/java/integration/AddressResolverIT.java
+++ lab2/lab2_2/gs-mockForHttpClient/src/test/java/integration/AddressResolverIT.java
@@ -1,59 +1,38 @@
 package integration;
 
+import connection.ISimpleHttpClient;
 import connection.TqsBasicHttpClient;
 import geocoding.Address;
 import geocoding.AddressResolver;
 import org.json.simple.parser.ParseException;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class AddressResolverIT {
 
-
-    @BeforeEach
-    public void init(){
-
-        private AddressResolver resolver;
-        private ISimpleHttpClient httpClient;
-        private String api_response = "{\"info\":{\"statuscode\":0,\"copyright\":{\"text\":\"\u00A9 2022 MapQuest, Inc.\",\"imageUrl\":\"http://api.mqcdn.com/res/mqlogo.gif\",\"imageAltText\":\"\u00A9 2022 MapQuest, Inc.\"},\"messages\":[]},\"options\":{\"maxResults\":1,\"thumbMaps\":true,\"ignoreLatLngInputv\":false},\"results\":[{\"providedLocation\":{\"latLng\":{\"lat\":40.6318,\"lng\":-8.658}},\"locations\":[{\"street\":\"Parque Estacionamento da Reitoria - Univerisdade de Aveiro\",\"adminArea6\":\"\",\"adminArea6Type\":\"Neighborhood\",\"adminArea5\":\"Gl\u00F3ria e Vera Cruz\",\"adminArea5Type\":\"City\",\"adminArea4\":\"\",\"adminArea4Type\":\"County\",\"adminArea3\":\"Centro\",\"adminArea3Type\":\"State\",\"adminArea1\":\"PT\",\"adminArea1Type\":\"Country\",\"postalCode\":\"3810-193\",\"geocodeQualityCode\":\"P1AAA\",\"geocodeQuality\":\"POINT\",\"dragPoint\":false,\"sideOfStreetv\":\"N\",\"linkId\":\"0\",\"unknownInput\":\"\",\"type\":\"s\",\"latLng\":{\"lat\":40.631803,\"lng\":-8.657881},\"displayLatLng\":{\"lat\":40.631803,\"lng\":-8.657881},\"mapUrl\":\"http://open.mapquestapi.com/staticmap/v5/map?key=uXSAVwYWbf9tJmsjEGHKKAo0gOjZfBLQ&type=map&size=225,160&locations=40.6318025,-8.657881|marker-sm-50318A-1&scalebar=true&zoom=15&rand=-472034917\",\"roadMetadata\":null}]}]}";
-    
-    }
+    private AddressResolver resolver = new AddressResolver( new TqsBasicHttpClient() );
 
     @Test
-    public void whenGoodCoordidates_returnAddress() throws IOException, URISyntaxException, ParseException {
+    void whenResolveAlboiGps_returnCaisAlboiAddress() throws ParseException, IOException, URISyntaxException {
 
-        //todo
-
-        // repeat the same tests conditions from AddressResolverTest, without mocks
-
-        // Mockito.when( httpClient.doHttpGet( anyString() ) ).thenReturn( this.api_response );
-
-        //e.g.
         Optional<Address> result = resolver.findAddressForLocation(40.640661, -8.656688);
 
-        //return
         assertEquals( result, Optional.of( new Address( "Cais do Alboi", "Glória e Vera Cruz", "Centro", "3800-246", null) ) );
 
     }
 
+    
     @Test
-    public void whenBadCoordidates_thenReturnNoValidAddrress() throws IOException, URISyntaxException, ParseException {
+    public void whenBadCoordidates_thenReturnNoValidAddress() throws IOException, URISyntaxException, ParseException {
 
-        //todo
-        // repeat the same tests conditions from AddressResolverTest, without mocks
+        assertThrows( IllegalArgumentException.class, () -> { resolver.findAddressForLocation(-300, -810); } );
 
-        //todo
-        // Mockito.when( httpClient.doHttpGet( anyString() ) ).thenReturn( this.api_response );
-
-        //e.g.
-        assertThrows( IllegalArgumentException.class, () -> { Optional<Address> result = resolver.findAddressForLocation(-300, -810); } );
-        
     }
 
 }
